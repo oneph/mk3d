ac2e02f50117097b2373563fd9e1d1f908902faa
dr.william.hornsby <dr.william.hornsby@d87eafa2-c07b-fe81-3635-4290edb905a9>
2011-03-16 20:51:06 +0000

    Update of Efield.h

--- inc/Efield.h
+++ inc/Efield.h
@@ -7,7 +7,26 @@
 
 using namespace std;
 
-void eadvance(complex<double>*****f,threevector ***B,int nmax,double dt,int gridx,int gridy,int gridz)//Use if monoenergetic due to enhanced stability of the centred trapezium scheme
+void eadvance_z(complex<double>*****f,threevector ***B,int nmax,double dt,int gridx,int gridy,int gridz)//Use if monoenergetic due to enhanced stability of the centred trapezium scheme
+{
+  int n,m,xpos,ypos,zpos;
+  double emrat = 1.0;//The ratio of the electic charge over the electron mass
+  complex<double> CompNul(0.0,0.0);
+  complex<double> i(0.0,1.0);
+  for(n=0;n<nmax;n++)//No need to do n=0,m=0 since df/dt = 0
+    {
+    	for(xpos=0;xpos<gridx;xpos++)
+	    	for(ypos=0;ypos<gridy;ypos++)
+	  			for(zpos=0;zpos<gridz;zpos++)
+	    			{
+	    			
+	    			
+	    			
+	    			}
+    }
+}
+
+void eadvance_xy(complex<double>*****f,threevector ***B,int nmax,double dt,int gridx,int gridy,int gridz)//Use if monoenergetic due to enhanced stability of the centred trapezium scheme
 {
   int n,m,xpos,ypos,zpos;
   double emrat = 1.0;//The ratio of the electic charge over the electron mass
